bf62e0c5063d8326a6b196397c4ba4391251cf14
Damien <damien.soen@gmail.com>
2022-01-20 09:35:20 +0100

    salon

---
+++ index.php
@@ -0,0 +1,84 @@
+<?php include 'includes/header.php' ?>
+
+
+<section>
+    <div class="container mt-5 text-center carousel">
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div id="carouselExampleInterval" class="carousel slide navUp" data-bs-ride="carousel">
+                    <div class="carousel-inner shadowCard">
+                        <div class="carousel-item active" data-bs-interval="3000">
+                            <a href=""> <img src="assets/img/cropped-fond-Flora-1.jpg" class="d-block w-100" alt="..."></a>
+                        </div>
+                        <div class="carousel-item" data-bs-interval="3000">
+                            <a href=""> <img src="assets/img/massage.jpg" class="d-block w-100" alt="..."> </a>
+                        </div>
+                        <div class="carousel-item" data-bs-interval="3000">
+                            <a href=""> <img src="assets/img/medi2.png" class="d-block w-100" alt="..."> </a>
+                        </div>
+                        <div class="carousel-item" data-bs-interval="3000">
+                            <a href=""> <img src="assets/img/boule.jpg" class="d-block w-100" alt="..."></a>
+                        </div>
+                        <div class="carousel-item" data-bs-interval="3000">
+                            <a href=""> <img src="assets/img/salon.jpg" class="d-block w-100" alt="..."></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<div class="container mt-5">
+    <div class="row">
+        <div class="col-sm-6 ms-auto me-auto">
+            <p>Le rendez vous incontournable des médecines douces propose 3 éditions 
+                <a href="https://www.google.com/maps/search/salon+du+bien+%C3%AAtre+paris/@48.8293951,2.1176853,11z" target="_blank">Paris</a>, 
+                <a href="https://www.google.com/maps/search/salon+du+bien+%C3%AAtre+Lyon/@45.7649347,4.7724823,12z/data=!3m1!4b1" target="_blank">Lyon</a> et
+                 <a href="https://www.google.com/maps/search/salon+du+bien+%C3%AAtre+marseille/@43.2959832,5.3572304,12.21z" target="_blank">Marseille</a>.
+                 <br> En 2022, l’évènement se recentre sur sa spécificité : un événement réunissant tous les acteurs et experts des médecines complémentaires :
+                    <br> Laboratoires spécialisés, écoles et formations en médecines douces et un village des thérapeutes.<br>
+
+                Un programme de conférences en écho avec l’actualité, une nouvelle implantation plus cohérente avec les attentes des visiteurs et 
+                des propositions d’expériences viendront compléter les fondamentaux du salon.
+
+            </p>
+
+        </div>
+    </div>
+</div>
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col-12 col-sm-5 text-center">
+            <div class="card btnUp shadowCard" style="width: 20rem;">
+                <img src="assets/img/prog.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Notre programme</h5>
+                    <p class="card-text"> Tu veux t'organiser correctement ?
+                        <br>Retrouve les programmes du salon ici !</p>
+                    <a type="button" target="_blank" href="programe.php" class="btn colorb btnUp">Aller voir</a>
+                </div>
+            </div>
+
+        </div>
+        <div class="col-12 col-sm-3 text-center">
+            <div class="card btnUp shadowCard" style="width: 20rem;">
+                <img src="assets/img/pouce.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Je participe</h5>
+                    <p class="card-text">Tu veux améliorer ton bien être ?
+                        <br> Ce salon est fait pour toi !
+                    </p>
+                    <a target="_blank" href="participe.php" class="btn colorb btnUp">Aller voir</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    AOS.init();
+</script>
+
+<?php include 'includes/footer.php' ?>
